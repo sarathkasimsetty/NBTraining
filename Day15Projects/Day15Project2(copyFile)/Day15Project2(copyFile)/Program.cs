63591680c00af015bcf87c7fc080e241b940cb3b
sarathkasimsetty <sarathkasimsetty18@gmail.com>
2022-02-14 02:03:02 +0530

    copyfile

---
+++ Day15Projects/Day15Project2(copyFile)/Day15Project2(copyFile)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15Project2_copyFile_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // string filesourcepath =@"F:\File data\quiz data.txt";
+           //  string filedestpath = @"D:\Backup data\taskschedular.txt";
+            
+
+            File.Copy("F:\\File data\\quiz data.txt", "D:\\Backup data\\taskschedular.txt");
+
+            Console.WriteLine("Backup success");
+
+            Console.ReadLine();
+        }
+    }
+}
