3cd3c44754c956273a21ea201613129fd0ce97e7
sarathkasimsetty <sarathkasimsetty18@gmail.com>
2022-02-20 18:10:14 +0530

    day 20

---
+++ Day20Projects/Project2(Delegate)/Project2(Delegate)/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//SARATH KASIMSETTY
+//Write C# code to illustrate the usage of delegates.
+
+namespace Project2_Delegate_
+{
+    public delegate void AlgebraCaller(int a, int b);
+    class Algebra
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine("Add : {0}",a + b);
+        }
+        public void Sub(int a, int b)
+        {
+            Console.WriteLine("Subtraction : {0}",a - b);
+        }
+        public void Mul(int a, int b)
+        {
+            Console.WriteLine("Multiple : {0}",a * b);
+        }
+        public void Mod(int a, int b)
+        {
+            Console.WriteLine("Modulo : {0}",a % b);
+        }
+    }
+    internal class Program
+    {    
+        static void Main(string[] args)
+        {
+            //creating class of object
+            Algebra algebra = new Algebra();
+            //Creating delegate of object 
+            //call from delegate all your methods
+            AlgebraCaller caller = new AlgebraCaller(algebra.Add);
+            caller += algebra.Sub;
+            caller += algebra.Mul;
+            caller += algebra.Mod;
+            //Call the methods of diference values
+            Console.WriteLine("Algebra of two numbers 5 and 6");
+            caller(5, 6);
+            Console.WriteLine("\n");
+            Console.WriteLine("Algebra of two numbers 15 and 10");
+            caller(15, 10);
+            Console.WriteLine("\n");
+            Console.WriteLine("Algebra of two numbers 24 and 12");
+            caller(24, 12);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day20Projects/Project3(Nullable)/Project3(Nullable)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//sarath kasimsetty
+
+namespace Project3_Nullable_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/*
+            int? bal_numA= null;
+            int? bal_numB = 200;
+            Console.WriteLine(bal_numA.GetValueOrDefault()+bal_numB.GetValueOrDefault());
+            Console.ReadLine();*/
+
+            int? input = null;
+            int b = 5;
+            if (input.HasValue)
+            {
+                Console.WriteLine(input * b);
+            }
+            else if (input.HasValue)
+                Console.WriteLine("input is null");
+            else
+            {
+                int output = input ?? 2;
+
+                Console.WriteLine(output);
+            }
+            Console.ReadLine();
+        }
+    }
+}
